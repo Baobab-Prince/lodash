dc31bac5aac0d577b0b46d51d32b51679f78118b
Chaz Carothers <chaz.carothers@gmail.com>
2022-01-18 23:23:32 -0600

    updated folder

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
-const numbers = [1, 2, 5, 2, 4, 1];
+// const myObj = {
+//   name: 'Chaz',
+//   age: '3000',
+//   location: 'Nashville',
+//   favBanc: 'Vulfpeck',
+// };
 
-const modifiedArray = _.fill(numbers, 7, 1, 10);
+// const objArr = [
+//   {
+//     name: 'Supreme',
+//     age: '61',
+//     location: 'Queens',
+//   },
+//   {
+//     name: 'Big U',
+//     age: '56',
+//     location: 'Los Angeles',
+//   },
+//   {
+//     name: 'Haitian Jack',
+//     age: '63',
+//     location: 'Haiti',
+//   },
+// ];
+// const numbers = [15, 22, 10, 14, 15, 10, 17];
 
-console.log(modifiedArray);
+let phrase = 'Once upon a time, in a land far far away...';
+
+console.log(_.startsWith(phrase, 'Once'));
+console.log(_.startsWith(phrase, 'once'));
+console.log(_.startsWith(phrase, 'land', 7));
+console.log(_.startsWith(phrase, 'land', 5));
+console.log(_.startsWith(phrase, 'away', 9));
